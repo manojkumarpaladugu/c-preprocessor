3c40ffa9ffc7d9e88eb496c6708f7706b1169352
manojkumarpaladugu <35074754+manojkumarpaladugu@users.noreply.github.com>
2018-03-18 16:17:46 +0530

    Add files via upload

---
+++ macro_replace.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void macro_replace(char *file)
+{
+	char buf[1000],*ptr,*directive="#define",macro_name[100],expansion[100];
+	int i,j;
+	FILE *fsrc,*ftar;
+	fsrc=fopen(file,"r");
+	ftar=fopen("temp.c","w+");
+	
+	//extract macro_name and value from file
+	while(fscanf(fsrc,"%s",buf)==1)
+	{
+	    if(strstr(buf,directive))
+	    {
+	        fscanf(fsrc,"%s",macro_name);
+		fscanf(fsrc,"%s",expansion);
+		break;
+	    }
+	}
+	rewind(fsrc);
+
+	//find and replace macro with value
+	while(fgets(buf,sizeof(buf),fsrc)!=NULL)
+	{
+	    if(strstr(buf,"#define"))
+	        continue;
+	    if(!strstr(buf,"printf"))
+	    {
+		if((ptr=strstr(buf,macro_name)))
+		{
+		    for(i=0;i<ptr-buf;i++)
+		        fputc(buf[i],ftar);
+		    for(j=0;j<strlen(expansion);j++)
+			fputc(expansion[j],ftar);
+		    for(i=ptr-buf+strlen(macro_name);i<strlen(buf);i++)
+			fputc(buf[i],ftar);
+		}
+		else
+		    for(i=0;i<strlen(buf);i++)
+			fputc(buf[i],ftar);
+	    }
+	    else
+		for(i=0;i<strlen(buf);i++)
+		    fputc(buf[i],ftar);
+	}
+
+	fclose(fsrc);
+	fclose(ftar);
+}
